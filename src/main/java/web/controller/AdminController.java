cf92c52f84c517a8f1ef2d7bb060d6c605bcb2f7
Pavel Mezin <57784396+PaulMezoni@users.noreply.github.com>
2021-06-21 22:41:54 +0300

    2.4.2_v3

---
+++ src/main/java/web/controller/AdminController.java
@@ -0,0 +1,75 @@
+package web.controller;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import web.dao.RoleDAO;
+import web.model.User;
+import web.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final UserService userService;
+    private final RoleDAO roleDAO;
+    private final PasswordEncoder passwordEncoder;
+
+    public AdminController(UserService userService, RoleDAO roleDAO, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.roleDAO = roleDAO;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public String allUsers(Model model) {
+        model.addAttribute("allUserList", userService.allUsers());
+        return "admin";
+    }
+
+    @GetMapping(value = "/new")
+    public ModelAndView newUserPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new");
+        modelAndView.addObject("user", new User());
+        modelAndView.addObject("roleList", roleDAO.getRoleSet());
+        return modelAndView;
+    }
+
+    @PostMapping(value = "")
+    public String newUserPost(@ModelAttribute("user") User user,
+                              @RequestParam("selectedRoles") String[] selectedRoles) {
+        userService.save(user, selectedRoles);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}")
+    public String showUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+        return "user";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editPage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+        model.addAttribute("rolesList", roleDAO.getRoleSet());
+        return "edit";
+    }
+
+    @PatchMapping(value = "/{id}")
+    public String editUserPatch(@ModelAttribute("user") User user, HttpServletRequest req) {
+        String[] selectedRoles = req.getParameterValues("selectedRoles");
+        userService.update(user, selectedRoles);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.delete(userService.getById(id));
+        return "redirect:/admin";
+    }
+
+}
--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,9 +1,11 @@
 package web.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import web.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,19 +14,25 @@ import java.util.List;
 @RequestMapping("/")
 public class UserController {
 
-	@RequestMapping(value = "hello", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC-SECURITY application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "hello";
-	}
+//	@RequestMapping(value = "hello", method = RequestMethod.GET)
+//	public String printWelcome(ModelMap model) {
+//		List<String> messages = new ArrayList<>();
+//		messages.add("Hello!");
+//		messages.add("I'm Spring MVC-SECURITY application");
+//		messages.add("5.2.0 version by sep'19 ");
+//		model.addAttribute("messages", messages);
+//		return "hello";
+//	}
 
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public String loginPage() {
-        return "login";
+    //    @RequestMapping(value = "login", method = RequestMethod.GET)
+//    public String loginPage() {
+//        return "login";
+//    }
+
+    @GetMapping
+    public String showUser(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
+        return "user";
     }
-
 }
